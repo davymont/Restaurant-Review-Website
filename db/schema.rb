794942983ed67babbd6deb28ae30aa17570bb6da
David Montgomery <davemont@outlook.com>
2020-09-30 18:52:09 +0100

    link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_165155) do
+ActiveRecord::Schema.define(version: 2020_09_30_174244) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_165155) do
     t.text "command"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
